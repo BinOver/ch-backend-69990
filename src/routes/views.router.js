49160c8055ba2a6ba91425ef5c5ce37799a94dd3
binover <binover@gmail.com>
2024-07-18 18:01:40 +0000

    Entrega Final - Agregar boton en productos para agregar a carrito

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 //import { ProductManager } from "../dao/fs/ProductManager.js";
 import ProductManager from "../dao/db/ProductManager-db.js";
+import CartManager from "../dao/db/CartManager-db.js";
 
 const routerViews = Router();
 const productManager = new ProductManager();
+const cartManager = new CartManager();
 
 routerViews.get("/", async (req, res) => {
     try {
@@ -18,8 +20,9 @@ routerViews.get("/", async (req, res) => {
 
         const productsDocs = products.docs.map(p => {
             const {_id, ...rest} = p.toObject();
-            return rest
+            return {_id, ...rest}
         });
+        console.log(productsDocs)
         res.render("home",{
             products:productsDocs,
             hasPrevPage: products.hasPrevPage, 
@@ -41,6 +44,30 @@ routerViews.get("/realtimeproducts", async (req, res) => {
         res.render("realtimeproducts");
     } catch (error) {
         res.status(500).send("Error interno del servidor");
+    }
+});
+
+routerViews.get("/carts/:cid", async (req, res) => {
+    try {
+        const { cid } = req.params;
+        const cart = await cartManager.getCartById(cid);
+        if (!cart) {
+            return res.status(404).send("Carrito no encontrado");
+        }
+        const cartProducts = cart.products.map(c => {
+            const { _id, ...rest } = c.toObject();
+            return rest;
+        });
+
+        console.log(cartProducts)
+
+        res.render("cart", {
+            cartId: cid,
+            products: cartProducts,
+        });
+
+    } catch (error) {
+        res.status(500).send("Error interno del servidor: " + error);
     }
 });
 
